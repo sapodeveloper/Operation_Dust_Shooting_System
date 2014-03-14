4db6cdf2c69d93371ea8157a2a616ebd8caa550e
morishita <tomohiro.m0219@gmail.com>
2014-03-15 00:22:44 +0900

    納品されているデザインサンプルを適用 classのuk-button-expandが廃止されているためuk-width-1-1に変更

---
+++ fuel/app/views/main/index.php
@@ -0,0 +1,96 @@
+<!-- 個人画面-->
+<center><h1 class="uk-article-title">TOP（教室選択）</h1></center>
+<div class="uk-alert uk-alert-danger uk-text-center" data-uk-alert>
+<a href="" class="uk-alert-close uk-close"></a>
+<i class="fa fa-exclamation-triangle"></i>&nbsp;&nbsp;
+	職員さんより「PC1は設備点検中です。」
+</div>
+<div class="uk-alert uk-text-center" data-uk-alert>
+<a href="" class="uk-alert-close uk-close"></a>
+<i class="fa fa-exclamation-circle"></i> &nbsp;&nbsp;
+	本日のオープン利用は「PC2、PC3、CAD1、CAD2、MM」です。
+</div>
+<br>
+<div class="uk-grid">
+<div class="uk-width-1-2">
+	<a href="03_Edit.html">
+		<button class="uk-button uk-button-primary uk-width-1-1 uk-button-large" >
+			<i class="fa fa-pencil-square-o"></i>&nbsp;編集画面
+		</button>
+	</a>
+</div>
+<div class="uk-width-1-2">
+	<a href="05_Report List.html">
+		<button class="uk-button uk-button-danger uk-width-1-1 uk-button-large" >
+			<i class="fa fa-pencil-square-o"></i>&nbsp;報告一覧&nbsp;&nbsp;
+			<i class="fa fa-exclamation-triangle"></i> 1
+		</button>
+	</a>
+</div>
+</div>
+<br>
+<div data-uk-scrollspy="{cls:'uk-animation-fade', delay:200, repeat: true}">
+<h3><center>↓&nbsp;から教室を選択して下さい。</center></h3>
+</div>
+<div class="uk-grid">
+	<div class="uk-width-medium-1-6">
+		<div class="uk-panel uk-panel-box" data-uk-scrollspy="{cls:'uk-animation-fade', delay:200, repeat: true}">
+			<a href="02_Input.html">
+				<button class="uk-button uk-button-success">
+					<p class="uk-text-center">PC1</p>
+					<p class="uk-text-left">前回の<br>作業日：1/1<br>作業者：<br>豊嶋</p>
+				</button>
+			</a>
+		</div>
+	</div>
+	<div class="uk-width-medium-1-6">
+		<div class="uk-panel uk-panel-box" data-uk-scrollspy="{cls:'uk-animation-fade', delay:300, repeat: true}">
+			<a href="02_Input.html">
+				<button class="uk-button uk-button-success">
+					<p class="uk-text-center">PC2</p>
+					<p class="uk-text-left">前回の<br>作業日：1/1<br>作業者：<br>三澤</p>
+				</button>
+			</a>
+		</div>
+	</div>
+	<div class="uk-width-medium-1-6">
+		<div class="uk-panel uk-panel-box" data-uk-scrollspy="{cls:'uk-animation-fade', delay:400, repeat: true}">
+			<a href="02_Input.html">
+				<button class="uk-button uk-button-success">
+					<p class="uk-text-center">PC3</p>
+					<p class="uk-text-left">前回の<br>作業日：1/1<br>作業者：<br>豊嶋</p>
+				</button>
+			</a>
+		</div>
+	</div>
+	<div class="uk-width-medium-1-6">
+		<div class="uk-panel uk-panel-box" data-uk-scrollspy="{cls:'uk-animation-fade', delay:500, repeat: true}">
+			<a href="02_Input.html">
+				<button class="uk-button uk-button-success">
+					<p class="uk-text-center">CAD1</p>
+					<p class="uk-text-left">前回の<br>作業日：1/1<br>作業者：<br>三澤</p>
+				</button>
+			</a>
+		</div>
+	</div>
+	<div class="uk-width-medium-1-6">
+		<div class="uk-panel uk-panel-box" data-uk-scrollspy="{cls:'uk-animation-fade', delay:600, repeat: true}">
+			<a href="02_Input.html">
+				<button class="uk-button uk-button-success">
+					<p class="uk-text-center">CAD2</p>
+					<p class="uk-text-left">前回の<br>作業日：1/1<br>作業者：<br>豊嶋</p>
+				</button>
+			</a>
+		</div>
+	</div>
+	<div class="uk-width-medium-1-6">
+		<div class="uk-panel uk-panel-box" data-uk-scrollspy="{cls:'uk-animation-fade', delay:700, repeat: true}">
+			<a href="02_Input.html">
+				<button class="uk-button uk-button-success">
+					<p class="uk-text-center">MM</p>
+					<p class="uk-text-left">前回の<br>作業日：1/1<br>作業者：<br>三澤</p>
+				</button>
+			</a>
+		</div>
+	</div>
+</div>
